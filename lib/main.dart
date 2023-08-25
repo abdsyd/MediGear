615efdf44071c690ba0b766b928a771b0bb97da0
Abd alsayad <abod.syd02@gmail.com>
2023-08-25 16:44:08 +0300

    added routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:hunter/views/login_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const LoginView(),
+
     );
   }
 }
