7a74d83c44a015ee8b297e746f63afc01d00192c
Abd alsayad <abod.syd02@gmail.com>
2023-08-28 11:21:42 +0300

    improved register_otp.dart

--- lib/views/register_otp.dart
+++ lib/views/register_otp.dart
@@ -12,58 +12,78 @@ import 'package:timer_count_down/timer_count_down.dart';
 class RegisterOTP extends StatelessWidget {
   const RegisterOTP({super.key});
 
-  //todo: implement timer and otp field
   @override
   Widget build(BuildContext context) {
     RegisterController rC = Get.find();
     return Scaffold(
+      //resizeToAvoidBottomInset: false,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
         leading: const BackButton(
           style: ButtonStyle(iconSize: MaterialStatePropertyAll(18)),
-        ),),
+        ),
+      ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 25),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              const AuthTitle(
-                title: 'Verification',
-                subTitle: 'please enter the code that sent to test@email.com \n this code will expire in ',
+              Column(
+                children: [
+                  const AuthTitle(
+                    title: 'Verification',
+                    subTitle:
+                        'please enter the code that sent to your email \n this code will expire in ',
+                  ),
+                  GetBuilder<RegisterController>(
+                    builder: (controller) => Countdown(
+                      controller: controller.timeController,
+                      seconds: 180,
+                      build: (context, double time) => Text(
+                        time.toString(),
+                        style: Theme.of(context)
+                            .textTheme
+                            .displayMedium!
+                            .copyWith(
+                                color: controller.isTimeUp
+                                    ? Colors.red
+                                    : AppColors.myPrimary),
+                      ),
+                      onFinished: () {
+                        controller.toggleTimerState(true);
+                      },
+                    ),
+                  ),
+                ],
               ),
-              Countdown(
-                controller: rC.timeController,
-                seconds: 180,
-                build: (_, double time) => Text(
-                  time.toString(),
-                  style: Theme.of(context).textTheme.displayMedium!.copyWith(color: rC.isTimeUp ? Colors.red : AppColors.myPrimary),
-                ),
-                onFinished: () {
-                  rC.toggleTimerState(true);
-                },
-              ),
+
               OTPTextField(
                 controller: rC.otpController,
                 length: 5,
-                width: MediaQuery.of(context).size.width ,
-                fieldWidth: MediaQuery.of(context).size.width /6.5,
-                style:Theme.of(context).textTheme.displayMedium!.copyWith(color: Colors.black),
+                width: MediaQuery.of(context).size.width,
+                fieldWidth: MediaQuery.of(context).size.width / 6.5,
+                style: Theme.of(context)
+                    .textTheme
+                    .displayMedium!
+                    .copyWith(color: Colors.black),
                 textFieldAlignment: MainAxisAlignment.spaceAround,
                 fieldStyle: FieldStyle.box,
                 otpFieldStyle: OtpFieldStyle(
                   focusBorderColor: AppColors.myPrimary,
                 ),
                 outlineBorderRadius: 15,
-                onCompleted: (pin) {
-                },
+                onCompleted: (pin) {},
                 //onChanged: (val){},
               ),
               //const SizedBox(height: 50,),
-              AuthButton(text: 'Verify', onPressed: (){}, color: AppColors.myPrimary),
-              AuthSuggestion(question: 'didn\'t receive a code? ', suggestion: 'resend', onTap: (){})
-
+              AuthButton(
+                  text: 'Verify', onPressed: () {}, color: AppColors.myPrimary),
+              AuthSuggestion(
+                  question: 'didn\'t receive a code? ',
+                  suggestion: 'resend',
+                  onTap: () {})
             ],
           ),
         ),
