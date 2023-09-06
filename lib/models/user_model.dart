e135a981a8f8c31a5f2799b94222c1ad45786353
Abd alsayad <abod.syd02@gmail.com>
2023-09-06 21:40:07 +0300

    added user_model.dart

---
+++ lib/models/user_model.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+UserModel userModelFromJson(String str) => UserModel.fromJson(json.decode(str));
+
+String userModelToJson(UserModel data) => json.encode(data.toJson());
+
+class UserModel {
+  String name;
+  String email;
+  String phone;
+  dynamic imgUrl;
+  String role;
+  bool isVerified;
+
+  UserModel({
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.imgUrl,
+    required this.role,
+    required this.isVerified,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+    name: json["name"],
+    email: json["email"],
+    phone: json["phone"],
+    imgUrl: json["img_url"],
+    role: json["role"],
+    isVerified: json["is_verified"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "name": name,
+    "email": email,
+    "phone": phone,
+    "img_url": imgUrl,
+    "role": role,
+    "is_verified": isVerified,
+  };
+}
